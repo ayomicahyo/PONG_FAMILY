e0b8c93aa734417a946aa06dbd8567dc6a38e99c
AlimanFZ <58335825+AlimanFZ@users.noreply.github.com>
2020-04-30 04:41:39 +0700

    Lobby. Part null

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -1,68 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject Main;
-    public GameObject StageSelect;
-    public GameObject Guide;
-    public GameObject DuelinOnePhone;
-    public GameObject DuelinDualPhone;
+    public GameObject mainMenu;
+    public GameObject typeGameplay;
 
-    public void Ready()
+    public void OpenTypeGameplay()
     {
-        if (Main.active == true)
+        if(mainMenu.active == true)
         {
-            StageSelect.SetActive(true);
-            Main.SetActive(false);
+            typeGameplay.SetActive(true);
+            mainMenu.SetActive(false);
         }
     }
 
-    public void OnePhone()
+    public void GameplayInOnePhone()
     {
-        if(DuelinOnePhone.active == false)
-        {
-            DuelinOnePhone.SetActive(true);
-            StageSelect.SetActive(false);
-            Main.SetActive(false);
-        }
+        SceneManager.LoadScene("PlayerSet");
     }
 
-    public void DualPhone()
+    public void BackButton()
     {
-        if (DuelinDualPhone.active == false)
+        if(typeGameplay.active == true)
         {
-            DuelinDualPhone.SetActive(true);
-            StageSelect.SetActive(false);
-            Main.SetActive(false);
+            mainMenu.SetActive(true);
+            typeGameplay.SetActive(false);
         }
     }
-
-    public void Exit()
-    {
-        if(DuelinOnePhone.active == true)
-        {
-            DuelinOnePhone.SetActive(false);
-            Main.SetActive(true);
-        }
-        else if (DuelinDualPhone.active == true)
-        {
-            DuelinDualPhone.SetActive(false);
-            Main.SetActive(true);
-        }
-
-        if(StageSelect.active == true)
-        {
-            StageSelect.SetActive(false);
-            Main.SetActive(true);
-        }
-
-        if(Guide.active == true)
-        {
-            Guide.SetActive(false);
-            Main.SetActive(true);
-        }
-    }
-
 }
