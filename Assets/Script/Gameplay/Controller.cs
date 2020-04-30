16d8f7250f9d886a595a3ca96918309126e6970c
ayomicahyo <62920255+ayomicahyo@users.noreply.github.com>
2020-05-01 06:48:59 +0700

    done fix
    benerin yang perlu dibenerin

--- Assets/Script/Gameplay/Controller.cs
+++ Assets/Script/Gameplay/Controller.cs
@@ -5,14 +5,55 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
 	private float deltaX,deltaY;
-	Vector2 mousePos;
+	//Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 	
-	void OnMouseDown(){
+	private void Update(){
+		
+		if(Input.touchCount > 0){
+			Touch touch1 = Input.GetTouch(0);
+			Vector2 touchPos1 = Camera.main.ScreenToWorldPoint(touch1.position);
+			
+			switch(touch1.phase){
+				case TouchPhase.Began:
+					if(GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos1)){
+						deltaX = touchPos1.x - transform.position.x;
+						deltaY = touchPos1.y - transform.position.y;
+					}
+					break;
+				
+				case TouchPhase.Moved:
+					if(GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos1)){
+						transform.position = new Vector2(touchPos1.x - deltaX , transform.position.y);
+					}
+					break;
+			}
+			
+			Touch touch2 = Input.GetTouch(1);
+			Vector2 touchPos2 = Camera.main.ScreenToWorldPoint(touch2.position);
+			
+			switch(touch2.phase){
+				case TouchPhase.Began:
+					if(GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos1)){
+						deltaX = touchPos2.x - transform.position.x;
+						deltaY = touchPos2.y - transform.position.y;
+					}
+					break;
+				
+				case TouchPhase.Moved:
+					if(GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos1)){
+						transform.position = new Vector2(touchPos2.x - deltaX , transform.position.y);
+					}
+					break;
+			}
+		}
+	}
+	
+	/*void OnMouseDown(){
 		deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
 		deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
 	}
@@ -20,5 +61,5 @@ public class Controller : MonoBehaviour
 	void OnMouseDrag(){
 		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		transform.position = new Vector2(mousePos.x - deltaX , transform.position.y);
-	}
+	}*/
 }
